f5608a6f69401ff2eb079dabbe25d7776795bfd6
Kara Gresham <proletarodactyl@proton.me>
2026-07-28 22:47:23 -0500

    added live instagram feed with sociablekit widget. updated endorsements.

--- js/main.js
+++ js/main.js
@@ -16,3 +16,14 @@ document.querySelectorAll('.issue-pill').forEach(pill => {
     this.classList.add('active');
   });
 });
+
+/* curator-feed-default-feed-layout */
+(function () {
+  var i, e, d = document, s = 'script';
+  i = d.createElement('script');
+  i.async = 1;
+  i.charset = 'UTF-8';
+  i.src = 'https://cdn.curator.io/published/8c7d7601-cf77-4348-b12d-25fc4543a5f3.js';
+  e = d.getElementsByTagName(s)[0];
+  e.parentNode.insertBefore(i, e);
+})();
